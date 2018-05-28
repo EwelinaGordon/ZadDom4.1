cea33d636fd03a2d8b8fe9a22ecbd52fd7df42cc
EwelinaGordon <ewelina.gordon@gmail.com>
2018-05-28 23:10:32 +0200

    Zadanie 4.1 usunelam konstruktor argumentowy (zg. z definicja z .pdf)

--- src/Triangle.java
+++ src/Triangle.java
@@ -27,27 +27,5 @@ public class Triangle {
         return bokC;
     }
 
-    void findMax(int a, int b, int c) {
-        if (a > b) {
-            if (a > c) {
-                max = a;
-                bokB = b;
-                bokC = c;
-            } else {
-                max = c;
-                bokB = a;
-                bokC = c;
-            }
-        } else if (b > c) {
-            max = b;
-            bokB = a;
-            bokC = c;
-        } else {
-            max = c;
-            bokB = a;
-            bokC = b;
-        }
 
-
-    }
 }
